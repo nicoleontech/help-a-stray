b7b39e32e8487ecfd909a2f7116e79c9da17651c
nicoleontech <nicole.sar.93@gmail.com>
2023-02-08 15:31:36 +0200

    initial commit

---
+++ src/main/java/net/sarrou/helpastray/HelpAStrayApplication.java
@@ -0,0 +1,13 @@
+package net.sarrou.helpastray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpAStrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpAStrayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sarrou/helpastray/HelpAStrayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sarrou.helpastray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpAStrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
